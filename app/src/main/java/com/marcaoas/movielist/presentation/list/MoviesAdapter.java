1b6fa5127c55bc17de23ef77f5c384af94c7bd54
Marco Souza <marcoasouza.1@gmail.com>
2017-03-01 10:49:42 -0400

    movie adapter

--- app/src/main/java/com/marcaoas/movielist/presentation/list/MoviesAdapter.java
+++ app/src/main/java/com/marcaoas/movielist/presentation/list/MoviesAdapter.java
@@ -1,26 +1,72 @@
 package com.marcaoas.movielist.presentation.list;
 
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.marcaoas.movielist.R;
+import com.marcaoas.movielist.domain.models.Movie;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * Created by marco on 28/02/17.
  */
 
-public class MoviesAdapter extends RecyclerView.Adapter {
+public class MoviesAdapter extends RecyclerView.Adapter<MoviesAdapter.MovieViewHolder> {
 
-    @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+    private ArrayList<Movie> movieList;
+
+    public MoviesAdapter() {
+        movieList = new ArrayList<>();
     }
 
     @Override
-    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+    public MovieViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.movie_list_item, parent, false);
+        return new MovieViewHolder(view);
+    }
 
+    public void onBindViewHolder(MovieViewHolder holder, int position) {
+        Movie movie = getItem(position);
+        holder.title.setText(movie.getTitle());
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return movieList.size();
+    }
+
+    public Movie getItem(int position) {
+        return movieList.get(position);
+    }
+
+    public void addMovies(List<Movie> movies) {
+        movieList.addAll(movies);
+        notifyDataSetChanged();
+    }
+
+    class MovieViewHolder extends RecyclerView.ViewHolder {
+        @BindView(R.id.movie_item_container)
+        View container;
+        @BindView(R.id.title_textView)
+        TextView title;
+        @BindView(R.id.popularity_textView)
+        TextView popularity;
+        @BindView(R.id.poster_imageView)
+        ImageView poster;
+
+        public MovieViewHolder(View view) {
+            super(view);
+            ButterKnife.bind(this, view);
+        }
     }
 }
